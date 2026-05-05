9e60e98e25cba4c7aad33bc9c1de702b6544b965
christiangeorgiou17 <christiangeorgiou17@gmail.com>
2026-05-05 14:28:07 +0100

    Update script.js
    Fixed bug - link not working

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function displayMeetingInformation() {
         if (info.link === "TBD") {
             linkInfo = "TBD";
         } else if (info.link.startsWith("https://calendar.app.google/")) {
-            linkInfo = `<a href="Click here to join">${info.link}</a>`;
+            linkInfo = `<a href="${info.link}">Click here to join</a>`;
         } else if (info.link.startsWith("https://drive.google.com/")) {
             linkInfo = `<a href="${info.link}" target="_blank">Click here to rewatch</a>`;
         } else {
