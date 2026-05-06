c59d5fb338f3128cb11c1b66de13892e136a7312
christiangeorgiou17 <christiangeorgiou17@gmail.com>
2026-05-06 08:40:41 +0100

    Refactor MEETING_INFORMATION structure

--- constants/meeting-information.js
+++ constants/meeting-information.js
@@ -22,7 +22,7 @@ export const MEETING_INFORMATION = {
     },
     PSYCHOLOGY: {
         name: "Psychology",
-        link: "TBD",
+        link: "https://calendar.app.google/Y3Mq4ZXm5cP2LG8U6",
         time: "May 6 14:30",
         paper: "1",
         presenter: "Dulansa"
