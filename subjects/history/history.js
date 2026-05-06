957380319cb7c09ed7e81a22947dcd071d0eb89d
christiangeorgiou17 <christiangeorgiou17@gmail.com>
2026-05-06 09:13:05 +0100

    Add files via upload

--- subjects/history/history.js
+++ subjects/history/history.js
@@ -1,5 +1,7 @@
-import { HISTORY_PAPER_1_EXAM_DATE, HISTORY_PAPER_2_EXAM_DATE } from "../../constants/exam-dates.js";
+import { HISTORY_PAPER_1A_EXAM_DATE, HISTORY_PAPER_1B_EXAM_DATE, HISTORY_PAPER_2A_EXAM_DATE, HISTORY_PAPER_2B_EXAM_DATE } from "../../constants/exam-dates.js";
 import { startCountdown } from "../../countdown.js";
 
-startCountdown(HISTORY_PAPER_1_EXAM_DATE, "COUNTDOWN_PAPER_1", "Paper 1: ");
-startCountdown(HISTORY_PAPER_2_EXAM_DATE, "COUNTDOWN_PAPER_2", "Paper 2: ");
+startCountdown(HISTORY_PAPER_1A_EXAM_DATE, "COUNTDOWN_PAPER_1A", "Paper 1A: ");
+startCountdown(HISTORY_PAPER_1B_EXAM_DATE, "COUNTDOWN_PAPER_1B", "Paper 1B: ");
+startCountdown(HISTORY_PAPER_2A_EXAM_DATE, "COUNTDOWN_PAPER_2A", "Paper 2A: ");
+startCountdown(HISTORY_PAPER_2B_EXAM_DATE, "COUNTDOWN_PAPER_2B", "Paper 2B: ");
