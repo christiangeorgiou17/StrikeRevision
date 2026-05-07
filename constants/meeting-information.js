8675898a52ea02f2a88a971cfadb78cd4b651141
christiangeorgiou17 <christiangeorgiou17@gmail.com>
2026-05-07 07:52:26 +0100

    Add files via upload

--- constants/meeting-information.js
+++ constants/meeting-information.js
@@ -4,48 +4,48 @@ export const MEETING_INFORMATION = {
         link: "https://drive.google.com/drive/folders/1VLINc1jm4yC2muyRYqd25gRoZ6XK6G_b?usp=sharing", // "https://calendar.app.google/Zxw1W5Z4RCW4WBXW7",
         time: "May 5 10:30",
         paper: "1",
-        presenter: "Christian"
+        host: "Christian"
     },
     COMPUTER_SCIENCE: {
         name: "Computer Science",
         link: "https://drive.google.com/drive/folders/16bqAoCrDV7aZYwsvwFljONrrfs4bFDCx?usp=drive_link", // "https://calendar.app.google/Ef98Acr2CphDjU1S9",
         time: "May 5 14:30",
         paper: "1",
-        presenter: "Christian"
+        host: "Christian"
     },
     GEOGRAPHY: {
         name: "Geography",
-        link: "https://drive.google.com/drive/folders/1J6J1chdxCI-Gpp1v8dfOnEmNLG2JwGaL?usp=drive_link", // "https://calendar.app.google/euhJUQqmp9josNDs8",
+        link: "https://drive.google.com/drive/folders/1J6J1chdxCI-Gpp1v8dfOnEmNLG2JwGaL?usp=sharing", // "https://calendar.app.google/euhJUQqmp9josNDs8",
         time: "May 6 10:30",
         paper: "1",
-        presenter: "Christian"
+        host: "Christian"
     },
     PSYCHOLOGY: {
         name: "Psychology",
         link: "https://calendar.app.google/Y3Mq4ZXm5cP2LG8U6",
         time: "May 6 14:30",
         paper: "1",
-        presenter: "Dulansa"
+        host: "Dulansa"
     },
     CHEMISTRY: {
         name: "Chemistry",
         link: "https://calendar.app.google/ck2eMW9nTaw64bYz9",
         time: "May 7 17:30",
         paper: "1",
-        presenter: "Christian"
+        host: "Christian"
     },
     MATHS: {
         name: "Mathematics",
         link: "TBD",
         time: "May 8 10:30",
         paper: "1H",
-        presenter: "Christian"
+        host: "Christian"
     },
     HISTORY: {
         name: "History",
         link: "TBD",
         time: "May 8 17:30",
         paper: "1",
-        presenter: "Erin"
+        host: "Erin"
     }
 };
